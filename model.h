599ad8db57a5d831b92188aec2aa1719709d6790
Kyle <admin@homecloudsoftware.com>
2015-11-03 20:12:35 -0800

    coming up with file format ideas

--- model.h
+++ model.h
@@ -1,9 +1,18 @@
 #include <QOpenGLBuffer>
 #include <QOpenGLVertexArrayObject>
+#include <vector>
+//#include cjson
+
+class Scene : public Scene {
+  bool unpacked = false;
+public:
+  Scene OpenScene(QString filename);
+  //OpenScene calls this -- void UnpackScene();
+  vector< Model > objects;
+};
 
 class Model : public Object {
 public:
-
   QOpenGLBuffer m_vertexBuffer;
   QOpenGLBuffer m_normalBuffer;
   QOpenGLBuffer m_textureUVBuffer;
